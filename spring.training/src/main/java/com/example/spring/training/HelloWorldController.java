2daa4791ddad4c02d19c9b5f4ebeefae53992fda
longbui98 <quanglongbui153@gmail.com>
2022-06-10 00:13:07 +0700

    update hello world controller

--- spring.training/src/main/java/com/example/spring/training/HelloWorldController.java
+++ spring.training/src/main/java/com/example/spring/training/HelloWorldController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	
 	@GetMapping(path="/hello-world")
-	public String helloWord() {
+	public String helloWorld() {
 		return "Hello World";
 	}
 	
 	@GetMapping(path="/hello-world-bean")
-	public HelloWorldBean helloWordBean() {
+	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
 }
