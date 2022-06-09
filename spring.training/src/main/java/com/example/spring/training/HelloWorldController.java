a162f0aa457d2a3ad28c2592bb4d832db036cf75
longbui98 <quanglongbui153@gmail.com>
2022-06-10 00:21:24 +0700

    implement hello world controller

---
+++ spring.training/src/main/java/com/example/spring/training/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.spring.training;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+}
