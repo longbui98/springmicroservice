77c21ed267a7e5355fa0d9932a904a4631d8df50
longbui98 <quanglongbui153@gmail.com>
2022-06-10 21:31:50 +0700

    implement path variable

--- spring.training/src/main/java/com/example/spring/training/HelloWorldController.java
+++ spring.training/src/main/java/com/example/spring/training/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.example.spring.training;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
