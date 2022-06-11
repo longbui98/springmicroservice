881044dc72c31cce68209f274b879d5661733867
longbui98 <quanglongbui153@gmail.com>
2022-06-11 09:22:18 +0700

    update create user resource

--- spring.training/src/main/java/com/example/spring/training/user/UserResource.java
+++ spring.training/src/main/java/com/example/spring/training/user/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,9 +24,8 @@ public class UserResource {
 
 	// Create User
 	@PostMapping(path = "/users")
-	public User createUser() {
-		User user = new User(4, "Python", new Date());
-		return service.save(user);
+	public void createUser(@RequestBody User user) {
+		service.save(user);
 	}
 
 	// Retrieve one User
